df5d11987e8831e503d8bb6917a3bff2552fe337
xinlong <623365323@qq.com>
2020-03-05 18:17:23 -0800

    damn

--- CS230-RigidBodySimulation/CS230-RigidBodySimulation/Shader.h
+++ CS230-RigidBodySimulation/CS230-RigidBodySimulation/Shader.h
@@ -1,9 +1,13 @@
 //
-//  init.cpp
+//  Shader.h
 //  CS230-RigidBodySimulation
 //
 //  Created by 易昕龙 on 2/29/20.
 //  Copyright © 2020 易昕龙. All rights reserved.
 //
 
-#include "init.hpp"
+#ifndef Shader_h
+#define Shader_h
+
+
+#endif /* Shader_h */
